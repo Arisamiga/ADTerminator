08d3fe7fd0c9f638c55561e227bb3c9e7ae05bcd
Aris <64918822+Arisamiga@users.noreply.github.com>
2022-06-21 16:12:44 +0100

    Added more Google ads removal

--- js/background.js
+++ js/background.js
@@ -5,7 +5,7 @@ chrome.runtime.onInstalled.addListener(() => {
 })
 
 
-chrome.webNavigation.onCompleted.addListener(({
+chrome.webNavigation.onDOMContentLoaded.addListener(({
   tabId
 }) => {
   chrome.scripting.executeScript({
@@ -17,11 +17,32 @@ chrome.webNavigation.onCompleted.addListener(({
 })
 
 async function newpage() {
+  // Normal Google ads
   var ads = document.getElementsByClassName("uEierd")
+  // Tophead Google ads
+  var topads = document.getElementsByClassName("Pm5mre")
+  // Side Google ads
+  var sideads = document.getElementsByClassName("cu-container")
   if (window.location.href.includes("www.google.com")) {
+    for (i in topads) {
+      try {
+        topads[0].remove()
+        chrome.storage.sync.set({removed: (await chrome.storage.sync.get("removed")).removed + 1})
+      } catch (err) {
+        // Tried to delete element that doesnt exist
+      }
+    }
     for (i in ads) {
       try {
         ads[0].remove()
+        chrome.storage.sync.set({removed: (await chrome.storage.sync.get("removed")).removed + 1})
+      } catch (err) {
+        // Tried to delete element that doesnt exist
+      }
+    }
+    for (i in sideads) {
+      try {
+        sideads[0].remove()
         chrome.storage.sync.set({removed: (await chrome.storage.sync.get("removed")).removed + 1})
       } catch (err) {
         // Tried to delete element that doesnt exist
--- js/counter_popup.js
+++ js/counter_popup.js
@@ -1,6 +1,5 @@
 async function add_counter() {
     var removed = await chrome.storage.sync.get("removed")
     document.getElementById("counter").innerHTML = removed.removed
-    console.log(removed)
 }
 add_counter()
