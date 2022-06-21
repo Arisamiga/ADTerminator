624dd8785394e2fbaa540047acbdaa897c9d357c
Aris <64918822+Arisamiga@users.noreply.github.com>
2022-06-21 16:26:05 +0100

    Removed Not needed Permissions + Updated to v1.1.1

--- js/newpage.js
+++ js/newpage.js
@@ -1,12 +1,39 @@
-var ads = document.getElementsByClassName("uEierd")
-async function rmads(){
-    for (i in ads) {
-        try {
-        ads[0].remove()
-        browser.storage.local.set({removed: (await chrome.storage.local.get("removed")).removed + 1})
-        } catch (err) {
+async function rmads() {
+  // Normal Google ads
+  var ads = document.getElementsByClassName("uEierd");
+  // Tophead Google ads
+  var topads = document.getElementsByClassName("Pm5mre");
+  // Side Google ads
+  var sideads = document.getElementsByClassName("cu-container");
+    for (i in topads) {
+      try {
+        topads[0].remove();
+        browser.storage.local.set({
+          removed: (await chrome.storage.local.get("removed")).removed + 1,
+        });
+      } catch (err) {
         // Tried to delete element that doesnt exist
-        }
+      }
+    }
+    for (i in ads) {
+      try {
+        ads[0].remove();
+        browser.storage.local.set({
+          removed: (await chrome.storage.local.get("removed")).removed + 1,
+        });
+      } catch (err) {
+        // Tried to delete element that doesnt exist
+      }
+    }
+    for (i in sideads) {
+      try {
+        sideads[0].remove();
+        browser.storage.local.set({
+          removed: (await chrome.storage.local.get("removed")).removed + 1,
+        });
+      } catch (err) {
+        // Tried to delete element that doesnt exist
+      }
     }
 }
-rmads()
+rmads();
