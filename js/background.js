b805657b40baf99f69f5f677db0ce066f2510bbc
Aris <64918822+Arisamiga@users.noreply.github.com>
2022-08-05 17:49:28 +0100

    Added Hovering on Ad Count to reveal details

--- js/background.js
+++ js/background.js
@@ -1,49 +1,74 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({
-    removed: 0
+    removed: 0,
+    removed_side: 0,
+    removed_top: 0,
+    removed_normal: 0,
   });
-})
+});
 
-
-chrome.webNavigation.onDOMContentLoaded.addListener(({
-  tabId
-}) => {
+chrome.webNavigation.onDOMContentLoaded.addListener(({ tabId }) => {
   chrome.scripting.executeScript({
     target: {
-      tabId
+      tabId,
     },
     function: newpage,
-  })
-})
+  });
+});
 
 async function newpage() {
   // Normal Google ads
-  var ads = document.getElementsByClassName("uEierd")
+  var ads = document.getElementsByClassName("uEierd");
   // Tophead Google ads
-  var topads = document.getElementsByClassName("Pm5mre")
+  var topads = document.getElementsByClassName("Pm5mre");
   // Side Google ads
-  var sideads = document.getElementsByClassName("cu-container")
+  var sideads = document.getElementsByClassName("cu-container");
   if (window.location.href.includes("www.google.com")) {
     for (i in topads) {
       try {
-        topads[0].remove()
-        chrome.storage.sync.set({removed: (await chrome.storage.sync.get("removed")).removed + 1})
+        topads[0].remove();
+        chrome.storage.sync.set({
+          removed: (await chrome.storage.sync.get("removed")).removed + 1,
+          removed_side: (await chrome.storage.sync.get("removed_side"))
+            .removed_side,
+          removed_top:
+            (await chrome.storage.sync.get("removed_top")).removed_top + 1,
+          removed_normal: (await chrome.storage.sync.get("removed_normal"))
+            .removed_normal,
+        });
       } catch (err) {
         // Tried to delete element that doesnt exist
       }
     }
     for (i in ads) {
       try {
-        ads[0].remove()
-        chrome.storage.sync.set({removed: (await chrome.storage.sync.get("removed")).removed + 1})
+        ads[0].remove();
+        chrome.storage.sync.set({
+          removed: (await chrome.storage.sync.get("removed")).removed + 1,
+          removed_side: (await chrome.storage.sync.get("removed_side"))
+            .removed_side,
+          removed_top: (await chrome.storage.sync.get("removed_top"))
+            .removed_top,
+          removed_normal:
+            (await chrome.storage.sync.get("removed_normal")).removed_normal +
+            1,
+        });
       } catch (err) {
         // Tried to delete element that doesnt exist
       }
     }
     for (i in sideads) {
       try {
-        sideads[0].remove()
-        chrome.storage.sync.set({removed: (await chrome.storage.sync.get("removed")).removed + 1})
+        sideads[0].remove();
+        chrome.storage.sync.set({
+          removed: (await chrome.storage.sync.get("removed")).removed + 1,
+          removed_side:
+            (await chrome.storage.sync.get("removed_side")).removed_side + 1,
+          removed_top: (await chrome.storage.sync.get("removed_top"))
+            .removed_top,
+          removed_normal: (await chrome.storage.sync.get("removed_normal"))
+            .removed_normal,
+        });
       } catch (err) {
         // Tried to delete element that doesnt exist
       }
--- js/counter_popup.js
+++ js/counter_popup.js
@@ -1,5 +1,17 @@
 async function add_counter() {
-    var removed = await chrome.storage.sync.get("removed")
-    document.getElementById("counter").innerHTML = removed.removed
+  var removed = await chrome.storage.sync.get("removed");
+  var removed_side = await chrome.storage.sync.get("removed_side");
+  var removed_top = await chrome.storage.sync.get("removed_top");
+  var removed_normal = await chrome.storage.sync.get("removed_normal");
+  document.getElementById("counter").innerHTML = removed.removed;
+  document.getElementById("types").innerHTML =
+    "Overall Ads: " +
+    removed.removed +
+    "<br> Side Ads: " +
+    removed_side.removed_side +
+    "<br> Top Ads: " +
+    removed_top.removed_top +
+    "<br> Search Ads: " +
+    removed_normal.removed_normal;
 }
-add_counter()
+add_counter();
