ca8f31c56704f902ddc6fbd27968bd69571d369d
Aris <64918822+Arisamiga@users.noreply.github.com>
2022-08-05 18:46:24 +0100

    Updated to meet Firefox Checklist

--- js/counter_popup.js
+++ js/counter_popup.js
@@ -1,17 +1,19 @@
 async function add_counter() {
-    var removed = await browser.storage.local.get("removed");
-    var removed_side = await browser.storage.local.get("removed_side");
-    var removed_top = await browser.storage.local.get("removed_top");
-    var removed_normal = await browser.storage.local.get("removed_normal");
-    document.getElementById("counter").textContent = removed.removed;
-    document.getElementById("types").innerHTML =
-      " Overall Ads: " +
-      removed.removed +
-      "<br> Side Ads: " +
-      removed_side.removed_side +
-      "<br> Top Ads: " +
-      removed_top.removed_top +
-      "<br> Search Ads: " +
-      removed_normal.removed_normal;
-  }
-  add_counter();
+  var removed = await browser.storage.local.get("removed");
+  var removed_side = await browser.storage.local.get("removed_side");
+  var removed_top = await browser.storage.local.get("removed_top");
+  var removed_normal = await browser.storage.local.get("removed_normal");
+  var element = document.createElement("div");
+  element.setAttribute("id", "types");
+  document.getElementById("counter").textContent = removed.removed;
+  element.textContent =
+    " Overall Ads: " + removed.removed + "\r\n";
+  element.textContent +=
+    " Side Ads: " + removed_side.removed_side + "\r\n";
+  element.textContent +=
+    " Top Ads: " + removed_top.removed_top + "\r\n";
+  element.textContent +=
+    " Search Ads: " + removed_normal.removed_normal + "\r\n";
+    document.getElementById("adsdetails").appendChild(element);
+}
+add_counter();
